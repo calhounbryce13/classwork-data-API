5a57e517abf5b47975cf72d26048bfed20efd7cb
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2025-06-01 21:04:34 -0700

    microservice A completed

---
+++ model.mjs
@@ -0,0 +1,235 @@
+
+import mongoose from 'mongoose';
+
+
+mongoose.connect(
+    //process.env.MONGODB_CONNECTION_STRING,
+    'mongodb+srv://calhounbryce13:T4113ngr10010137@mycluster1.iv9rj.mongodb.net/school_stuff?retryWrites=true&w=majority&appName=MyCluster1',
+    { useNewUrlParser: true }
+);
+
+const db = mongoose.connection;
+
+db.once("open", ()=>{
+    console.log("\nconnected to mongodb database!");
+});
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+const task = new mongoose.Schema({
+    title: String,
+    description: String,
+    due_date: String,
+    due_time: String,
+    week: String
+});
+
+const event = new mongoose.Schema({
+    title: String,
+    location: String,
+    date: String,
+    start_time: String,
+    end_time: String,
+    week: String
+});
+
+const classSchema = new mongoose.Schema({
+    title: String,
+    location: String,
+    room: String,
+    class_date: String,
+    start_time: String,
+    end_time: String,
+    week: String
+});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    created_at: String,
+    classes: [classSchema],
+    events: [event],
+    tasks: [task]
+});
+
+const userModel = mongoose.model('user', userSchema, 'microservice_A');
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+const testClasses = [
+    {
+        title: "test class",
+        location: "test class location",
+        room: "room number",
+        class_date: "2025-05-06",
+        start_time: "22:12:00",
+        end_time: "22:12:00"
+    }
+]
+
+const testEvents = [
+    {
+        title: "event 1",
+        location: "location task",
+        date: "2025-05-06",
+        start_time: "22:12:00",
+        end_time: "22:12:00"
+    }
+]
+
+const testTasks = [
+    {
+        title: "task",
+        description: "descrition",
+        due_date: "2025-05-06",
+        due_time: "22:12:00"
+    }
+]
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+const extract_month = function(dateString){
+    let month = '';
+    let isMonth = false;
+    for(let i = 0; i < dateString.length; i++){
+        if(dateString[i] == '-' && (!isMonth)){
+            isMonth = true;
+            continue;
+        }
+        if(isMonth){
+            if(dateString[i] == '-'){
+                return month;
+            }
+            else{
+                month += dateString[i];
+            }
+            
+        }
+        
+    }
+
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+const create_new_user = async(email, password, created_at, classes, events, tasks)=>{
+    const newUser = new userModel({email: email, password:password, created_at: created_at, classes: classes, events: events, tasks: tasks});
+
+    return await newUser.save();
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+const get_daily_data = async(id, date)=>{
+    const myUser = await userModel.findById(id);
+    if(myUser){
+        const returnObject = {
+            classes: [],
+            events: [],
+            tasks: []
+        };
+
+        for(let i = 0; i < (myUser.classes).length; i++){
+            if(myUser.classes[i].class_date == date){
+                (returnObject.classes).push(myUser.classes[i]);
+            }
+        }
+
+        for(let i = 0; i < (myUser.events).length; i++){
+            if(myUser.events[i].date == date){
+                (returnObject.events).push(myUser.events[i]);
+            }
+        }
+
+        for(let i = 0; i < (myUser.tasks).length; i++){
+            if(myUser.tasks[i].due_date == date){
+                (returnObject.tasks).push(myUser.tasks[i]);
+            }
+        }
+        return returnObject;
+    }
+    return null;
+}
+
+const get_monthly_data = async(id, date)=>{
+    const myUser = await userModel.findById(id);
+    if(myUser){
+        const returnObject = {
+            classes: [],
+            events: [],
+            tasks: []
+        };
+        for(let i = 0; i < (myUser.classes).length; i++){
+            const givenDate = myUser.classes[i].class_date;
+            if(extract_month(givenDate) == extract_month(date)){
+                returnObject.classes.push(myUser.classes[i]);
+            }
+        }
+        for(let i = 0; i < (myUser.events).length; i++){
+            const givenDate = myUser.events[i].date;
+            if(extract_month(givenDate) == extract_month(date)){
+                returnObject.events.push(myUser.events[i]);
+            }
+            
+        }
+        for(let i = 0; i < (myUser.tasks).length; i++){
+            const givenDate = myUser.tasks[i].due_date;
+            if(extract_month(givenDate) == extract_month(date)){
+                returnObject.tasks.push(myUser.tasks[i]);
+            }
+        }
+        return returnObject;
+    }
+    return null;
+}
+
+const get_weekly_data = async(id, date, week)=>{
+    const myUser = await userModel.findById(id);
+    if(myUser){
+        const returnObject = {
+            classes: [],
+            events: [],
+            tasks: []
+        };
+        for(let i = 0; i < (myUser.classes).length; i++){
+            const givenDate = myUser.classes[i].class_date;
+            if((extract_month(givenDate) == extract_month(date)) && (week == myUser.classes[i].week)){
+                returnObject.classes.push(myUser.classes[i]);
+            }
+        }
+        for(let i = 0; i < (myUser.events).length; i++){
+            const givenDate = myUser.events[i].date;
+            if((extract_month(givenDate) == extract_month(date)) && (week == myUser.events[i].week)){
+                returnObject.events.push(myUser.events[i]);
+            }
+            
+        }
+        for(let i = 0; i < (myUser.tasks).length; i++){
+            const givenDate = myUser.tasks[i].due_date;
+            if((extract_month(givenDate) == extract_month(date)) && (week == myUser.tasks[i].week)){
+                returnObject.tasks.push(myUser.tasks[i]);
+            }
+        }
+        return returnObject;
+    }
+    return null;
+}
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+export default { get_daily_data, get_monthly_data, get_weekly_data }
+
+
+
+
--- server.js
+++ server.js
@@ -1,42 +1,56 @@
 /*
-Description: Microservice for Xaviver's frontend main program. Endpoints to return 
+Description: Microservice A for Xaviver's frontend main program. Endpoint to return 
             user data from a mongoDB database.
 Author: Bryce Calhoun
 */
 
 
 import express from 'express';
-import User from './model.js';
+import User from './model.mjs';
 const app = express();
 const PORT = 3000;
 
 
 app.use(express.json());
-
-//? what are the endpoints that I need to build?
-//todo 1 request for daily, weekly, or monthly class data
+app.use(express.urlencoded({extended: true}));
 
 
-app.get('/user-view', (req, res)=>{
-    const { type, date, id } = req.body;
-    if(type == 'weekly' || type == 'daily' || type == 'monthly'){
-        try{
-            User.get_user_data(type, date, id);
-
-        }catch(error){
-            console.log(error);
-            res.status(500).json({"Error": "Internal server error"});
+app.post('/user-view', async(req, res)=>{
+    const { type, date, user_id } = req.body;
+    if(type && date && user_id){
+        if(type == 'weekly' || type == 'daily' || type == 'monthly'){
+            try{
+                if(type === 'daily'){
+                    const data = await User.get_daily_data(user_id, date);
+                    res.status(200).json(data);
+                    console.log("wnefiown");
+                    return;
+                }
+                else if(type == 'weekly'){
+                    if(!(req.body.week)){
+                        res.status(400).json({"Error": "Bad Request"});
+                        return;
+                    }
+                    const data = await User.get_weekly_data(user_id, date, req.body.week);
+                    res.status(200).json(data);
+                    return;
+                }
+                else{
+                    const data = await User.get_monthly_data(user_id, date);
+                    res.status(200).json(data);
+                    return;
+                }
+            }catch(error){
+                console.log(error);
+                res.status(500).json({"Error": "Internal server error"});
+                return;
+            }
         }
     }
-    else{
-        res.status(400).json({"Error": "Bad request"});
-    }
-    
-
-
+    res.status(400).json({"Error": "Bad request"});
 });
 
 
-app.listen(()=>{
+app.listen(PORT, ()=>{
     console.log("app listening on port: ", PORT);
 });
